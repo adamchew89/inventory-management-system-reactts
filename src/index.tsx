7a97452e6ae287d3eb7641dc3b0455fc2c632d21
Chew <uidn3250@contiwan.com>
2020-12-24 15:04:14 +0800

    Added @material-ui configs

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "@material-ui/core/styles";
 // CSS
+import theme from "./configs/material-ui/theme";
 import "./index.css";
 // Reports
 import reportWebVitals from "./reportWebVitals";
@@ -14,7 +16,9 @@ import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
